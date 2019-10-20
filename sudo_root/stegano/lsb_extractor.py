5d44e20b359b28d4626320e24075b34bec5de2a2
philomath213 <bilalphilomath@gmail.com>
2019-10-20 13:04:13 +0100

    refactore lsb_extractor.py

--- sudo_root/stegano/lsb_extractor.py
+++ sudo_root/stegano/lsb_extractor.py
@@ -2,30 +2,29 @@
 
 from PIL import Image
 
-class lsb_extractor(object):
+
+class LSBExtractor(object):
     """docstring for lsb_extractor."""
+
     def __init__(self, img_path):
-        try:
-            self.img = Image.open(img_path)
-            self.data = list(self.img.getdata())
-        except Exception as e:
-            raise
+        self.img = Image.open(img_path)
+        self.data = list(self.img.getdata())
 
     def all_lsb(self, length):
         out = ""
-        for i in xrange(min(len(self.data), length)):
-            out += "".join(map(lambda x:str(x & 1), self.data[i]))
+        for i in range(min(len(self.data), length)):
+            out += "".join(map(lambda x: str(x & 1), self.data[i]))
         return out
 
     def cycle_lsb(self, length):
         out = ""
-        for i in xrange(min(len(self.data), length)):
-                out += str(self.data[i][i % 3] & 1)
+        for i in range(min(len(self.data), length)):
+            out += str(self.data[i][i % 3] & 1)
         return out
 
     @staticmethod
     def bits2bytes(bits):
         out = ""
-        for i in xrange(0,len(bits)/8, 8):
+        for i in range(0, len(bits)/8, 8):
             out += chr(int(bits[i:i+8], 2))
         return out
