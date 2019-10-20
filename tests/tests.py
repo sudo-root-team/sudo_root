b4d602538fa7181cbc7344f5c57a78d0065c560b
philomath213 <bilalphilomath@gmail.com>
2019-10-20 12:58:42 +0100

    add LSBExtractor unittests

---
+++ tests/tests.py
@@ -0,0 +1,33 @@
+import unittest
+from sudo_root.stegano import LSBExtractor
+
+
+class TestLSBExtractor(unittest.TestCase):
+
+    def test_all_lsb(self):
+        img_path = "./test_assets/stegano/lamiri_sba.png"
+
+        lsb = LSBExtractor(img_path)
+        out = lsb.all_lsb(40)
+
+        msg = ""
+        for i in range(0, len(out), 8):
+            msg += chr(int(out[i:i+8], 2))
+
+        self.assertEqual(msg, "Hello World !!!")
+
+    def test_all_lsb(self):
+        img_path = "./test_assets/stegano/esi_sba_lsb_cycle.png"
+
+        lsb = LSBExtractor(img_path)
+        out = lsb.cycle_lsb(192)
+
+        msg = ""
+        for i in range(0, len(out), 8):
+            msg += chr(int(out[i:i+8], 2))
+
+        self.assertEqual(msg, "LSB cycle hidden message")
+
+
+if __name__ == '__main__':
+    unittest.main()
