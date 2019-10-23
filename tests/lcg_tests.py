da9b73f07ddb0aa524fa11ef775541b68e707ffa
philomath213 <bilalphilomath@gmail.com>
2019-10-23 15:09:19 +0100

    remove old test files

--- tests/lcg_tests.py
+++
@@ -1,6 +0,0 @@
-from sudo_root.crypto import lcg
-
-def break_lcg_test():
-    l = lcg.Lcg(0x66e158441b6995, 0xB, 1<<85, 53)
-    state = l.get_state(2752470789, 3367609997,1185935283)
-    l.next()
